2717c08a0a31e0d8838e713551d6044fe1bc1373
Kaio Rosa <kaiosrosa@gmail.com>
2021-06-29 19:40:19 -0300

    add transfer funds functionality

--- src/modules/statements/mappers/BalanceMap.ts
+++ src/modules/statements/mappers/BalanceMap.ts
@@ -1,24 +1,44 @@
 import { Statement } from "../entities/Statement";
 
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer'
+}
+
 export class BalanceMap {
   static toDTO({statement, balance}: { statement: Statement[], balance: number}) {
     const parsedStatement = statement.map(({
       id,
       amount,
+      user_id,
       description,
       type,
       created_at,
       updated_at
-    }) => (
-      {
-        id,
-        amount: Number(amount),
-        description,
-        type,
-        created_at,
-        updated_at
+    }) => {
+      if(type !== OperationType.TRANSFER){
+        return {
+          id,
+          amount: Number(amount),
+          description,
+          type,
+          created_at,
+          updated_at
+        }
       }
-    ));
+      else {
+        return {
+          id,
+          sender_id: user_id,
+          amount: Number(amount),
+          description,
+          type,
+          created_at,
+          updated_at
+        }
+      }
+    });
 
     return {
       statement: parsedStatement,
